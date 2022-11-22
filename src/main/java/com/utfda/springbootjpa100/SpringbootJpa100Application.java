d11d7f249c327f38767765c1e5b9fc19a4ff3e01
dlworhd <dlwodud821@gmail.com>
2022-11-22 11:02:51 +0900

    init

---
+++ src/main/java/com/utfda/springbootjpa100/SpringbootJpa100Application.java
@@ -0,0 +1,13 @@
+package com.utfda.springbootjpa100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpa100Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpa100Application.class, args);
+    }
+
+}
